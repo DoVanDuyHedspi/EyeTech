301da803e6edeb88af0e12baf88c604ed4c7eeea
Nguyen Thanh Tung <tungbk.devjv@gmail.com>
2018-12-13 12:30:15 +0700

    fix api-events-format

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Camera;
 use App\Customer;
 use App\Http\Requests\EventFormatFormRequest;
 use App\Http\Requests\EventFormRequest;
@@ -197,24 +198,26 @@ class EventController extends Controller
         foreach ($events as $event)
         {
             $customer = Customer::find($event->customer_id);
+            $camera = Camera::findOrFail($event->camera_id);
 
             $numberImageCamera = 2;
             $numberImageDetection = 2;
             $slice_image_camera = $this->handleImage($event->image_camera_url_array, $numberImageCamera);
             $slice_image_detection = $this->handleImage($event->image_detection_url_array, $numberImageDetection);
+            $avatar = $this->checkImageNull($event->image_camera_url_array[0]);
 
             $timeInDefault = $event->time_in;
             $timeInHandle = $this->handleTimeIn($timeInDefault);
 
             $eventFormat = [
-                'branch_id' => $branch_id,
                 'customer_id' => $customer->id,
+                'avatar' => $avatar,
                 'name' => $customer->name,
                 'type' => $customer->type,
-                'time_in' => $timeInHandle,
-                'favorites' => $customer->favorites,
+                'camera' => $camera->name,
                 'image_camera_url_array' => $slice_image_camera,
                 'image_detection_url_array' => $slice_image_detection,
+                'time_in' => $timeInHandle,
             ];
             array_push($data, $eventFormat);
         }
