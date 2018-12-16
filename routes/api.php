25f48f7f12a4b13e3940b956ab0bd0db2bce56c1
Nguyen Thanh Tung <tungbk.devjv@gmail.com>
2018-12-16 19:26:23 +0700

    create API list-store-for-client

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ Route::group(['prefix' => 'v1'], function() {
     Route::post('users/register', 'Api\UserController@store');
     Route::post('users/login', 'Api\UserController@login');
     Route::get('list-stores-id', 'Api\Admin\StoreController@getStoreID');
+    Route::get('list-stores-client', 'Api\Admin\StoreController@getStoreForClient');
     Route::post('list-branches-id', 'Api\Admin\BranchController@getBranchID');
     Route::post('events-format', 'Api\EventController@formatEventForClient');
     Route::post('quick-events-format', 'Api\EventController@formatQuickEventForClient');
