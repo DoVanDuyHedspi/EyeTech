b023452010e49401221a4910e7a2384bd8037576
Nguyen Thanh Tung <tungbk.devjv@gmail.com>
2018-12-19 04:36:18 +0700

    create insert image

--- app/Http/Controllers/Api/GalleryController.php
+++ app/Http/Controllers/Api/GalleryController.php
@@ -118,10 +118,6 @@ class GalleryController extends Controller
             $image->move(public_path().'/images', $name);
         }
 
-        $image= new Image();
-        $image->image_name = $name;
-        $image->save();
-
         return response()->json(['success' => 'You have successfully uploaded an image'], 200);
 
 
