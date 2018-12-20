209c6122d6d19723300b4ecb0b7e4fe850d0faca
Nguyen Thanh Tung <tungbk.devjv@gmail.com>
2018-12-20 15:35:36 +0700

    fix

--- app/Http/Controllers/Api/GalleryController.php
+++ app/Http/Controllers/Api/GalleryController.php
@@ -282,7 +282,6 @@ class GalleryController extends Controller
         $customer->image_url_array = $this->generateImagesUrl($customer_id, $new_image_base64);
         $customer->vector = $data->vector;
         $customer->save();
-        print_r($new_image);
     }
 
 }
